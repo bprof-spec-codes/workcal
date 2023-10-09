f5baec80d28ebb7d83b5baa347debacc574009ba
Bognár Bence <bbence210@gmail.com>
2023-10-09 21:15:52 +0200

    Bugfix: removing all labels

--- workcal/angular/src/app/calendar-page/calendar-page.component.ts
+++ workcal/angular/src/app/calendar-page/calendar-page.component.ts
@@ -146,75 +146,36 @@ IdLabels: Array<{ name: string, color: string,eventId: string }> = [
       return;
     }
 
+    // Initialize labels to empty arrays if they are null or undefined
+    appointmentData.labels = appointmentData.labels || [];
+    appointmentDataOld.labels = appointmentDataOld.labels || [];
 
+    let selectedLabels = [];
 
-    //  label data
-    /*const selectedLabels = this.IdLabels.filter(label =>
-      appointmentData.labels?.includes(label.name)
-    ).map(label => {
-      label.eventId = appointmentDataOld.id;  // Add EventId to each selected label
-      return label;
-    });*/
-
-
-   // Prepare label data
-   let selectedLabels = [];
-
-  // Check if labels are provided and if they are strings (from form) or objects (from drag-and-drop)
-  if (appointmentData.labels && appointmentData.labels.length > 0) {
-    if (typeof appointmentData.labels[0] === 'string') {
-      // If from form, labels are strings
+    // Check if the labels field has been interacted with
+    if (this.labelsInteractedWith) {
+      // Trust the new labels from the form
       selectedLabels = this.defaultLabels.filter(label => appointmentData.labels.includes(label.name));
     } else {
-      // If from drag-and-drop, labels are objects
-      selectedLabels = this.defaultLabels.filter(label => appointmentData.labels.map(l => l.name).includes(label.name));
+      // Use the old labels
+      selectedLabels = appointmentDataOld.labels;
     }
-  } else {
-    // If no new labels are provided, retain the old labels
-    selectedLabels = appointmentDataOld.labels;
+
+    const updatedEvent: EventDto = {
+      id: appointmentDataOld.id,
+      name: appointmentData.text,
+      startTime: new Date(appointmentData.startDate),
+      endTime: new Date(appointmentData.endDate),
+      location: appointmentData.location || '',
+      labels: selectedLabels  // Set selectedLabels, which may be empty
+    };
+
+    console.log("Prepared payload for updating:", updatedEvent);
+    this.updateEvent(updatedEvent);
+    this.labelsInteractedWith = false;
+
   }
 
-
-  // Prepare label data
-   // Prepare label data only if it's not already set
-   //let selectedLabels = appointmentData.labels;
-   console.log("selectedLabels:", selectedLabels);
-
-  /*if (selectedLabels && selectedLabels.length > 0) {
-    // Map label names back to full label objects
-    selectedLabels = this.defaultLabels.filter(label =>
-      selectedLabels.includes(label.name)
-    );
-    console.log("selectedLabels: > 0", selectedLabels);
-
-  } else {
-    // If no labels are set, select labels from defaultLabels
-    selectedLabels = this.defaultLabels.filter(label =>
-      appointmentDataOld.labels?.includes(label.name)
-    );
-  }*/
- /*const selectedLabels = appointmentData.labels?[0].length > 0 ?
-    this.defaultLabels.filter(label => appointmentData.labels.includes(label.name)) :
-    appointmentDataOld.labels;*/
-
-
-
-    console.log("selectedLabels: ", selectedLabels);
-
-
-
-   const eventToUpdate: EventDto = {
-     id: appointmentDataOld.id,
-     name: appointmentData.text,
-     startTime: new Date(appointmentData.startDate),
-     endTime: new Date(appointmentData.endDate),
-     location: appointmentData.location || '',
-     labels: selectedLabels
-   };
-
-  console.log("Prepared payload for updating:", eventToUpdate);
-  this.updateEvent(eventToUpdate);
-}
 
 
   onEventDeleting(event: any): void {
@@ -232,6 +193,8 @@ IdLabels: Array<{ name: string, color: string,eventId: string }> = [
     this.deleteEvent(appointmentData.id);
   }
 
+// Class-level variable to hold the flag
+labelsInteractedWith: boolean = false;
 
   onAppointmentFormOpening(data: any): void {
     const form = data.form;
@@ -274,6 +237,10 @@ IdLabels: Array<{ name: string, color: string,eventId: string }> = [
           }
         }
       ]
+    });
+
+    form.getEditor('labels').option('onValueChanged', () => {
+      this.labelsInteractedWith = true;
     });
   }
 
